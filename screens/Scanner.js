1eaa01e74cd8e61f296181e0b4b88b41a12ff78c
root <root@PortDavid.velfairdc.local>
2022-08-12 14:47:12 +0200

    first commit

--- screens/Scanner.js
+++ screens/Scanner.js
@@ -5,6 +5,7 @@ import { BarCodeScanner } from 'expo-barcode-scanner';
 export default function Scanner(){
     const [hasPermission, setHasPermission] = useState(null);
     const [scanned, setScanned] = useState(false);
+    const [id, setId] = useState('')
 
     useEffect(() => {
         (async() => {
@@ -15,7 +16,8 @@ export default function Scanner(){
 
     const handleBarCodeScanned = ({ type, data}) => {
         setScanned(true);
-        alert(`Bar Code With Type ${type} and data ${Linking.openURL(`${data}`)} has been scanned`);
+        setId(data);
+        /*alert(`Qr Code With Type ${type} and data ${Linking.openURL(`${data}`)} has been scanned`);*/
     };
 
     if(hasPermission === null) {
@@ -28,13 +30,14 @@ export default function Scanner(){
 
     return (
         <View style={styles.container}> 
-            <BarCodeScanner
-                onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-                style = {StyleSheet.absoluteFillObject}
+                <BarCodeScanner
+                 onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+                 style = {StyleSheet.absoluteFillObject}
 
-            />
-            {scanned && <Button title='Tap to Scan Again' onPress={() => setScanned(false)} />}
+                />
+                {scanned && <Button title='Tap to Scan Again' onPress={() => setScanned(false)} />}
         </View>
+        
     ) 
     
 }
@@ -44,5 +47,5 @@ const styles = StyleSheet.create({
         flex: 1,
         flexDirection: 'column',
         justifyContent:'center',
-    }
+    },
 })
